e2ccd5fa34957fc8a6877017c7dc2d383c55eda0
Art Ardolino <Art.Ardolino@eis.consulting>
2022-09-17 16:52:55 -0500

    Adding parsing for iBeacons.  Gateway must broadcast data in JSON.

--- app.groovy
+++ app.groovy
@@ -31,13 +31,11 @@ def mainPage() {
     String uri = makeUri("/");
     return dynamicPage(name: "mainPage", title: "BLE Gateway Manager", uninstall: true, install: true) {
         section(){ 
-            paragraph("This app uses an <a href=\"https://store.aprbrother.com/product/ab-ble-gateway-4-0\" target=\"_blank\">April Brother BLE Gateway (v4)</a> to associate beacons with Virtual Presence devices to automate events.")
-			input "gateway", "capability.presenceSensor", title: "BLE Gateway", multiple: false, required: true
-			href "addBeaconPage", title: "<b>Add New Beacon Devices</b>", description: "Add New Beacon Devices."
-			href "listBeaconsPage", title: "<b>List Beacon Devices</b>", description: "List All Beacon Devices."
-
-            app(name: "anyOpenApp", appName: "April Brother BLE Beacon", namespace: "ajardolino3", title: "<b>Add a new Bluetooth Beacon</b>", multiple: true)
-            paragraph("Gateway URL: <a href='${uri}'>${uri}</a>")
+            paragraph("The BLE Gateway Manager processes data from a Bluetooth (BLE) Gateway to detect the presence of beacons.")
+			input "gateway", "capability.presenceSensor", title: "Select your BLE Gateway Device", multiple: false, required: true
+            paragraph("Configure your gateway to use this URL: <a href='${uri}'>${uri}</a>")
+			href "addBeaconPage", title: "<b>Add Beacons</b>", description: "Adds new beacon devices."
+			href "listBeaconsPage", title: "<b>List Beacons</b>", description: "Lists added beacon devices."
 			input "debugLog", "bool", title: "Enable debug logging", submitOnChange: true, defaultValue: false
         }
     }
@@ -47,10 +45,11 @@ def listBeaconsPage() {
     def devices = getChildDevices()
     def message = ""
     devices.each{ beacon ->
-        message += "\t${beacon.label} (${beacon.name})\n"
+        message += "\t${beacon.label} (${beacon.name}, Network ID: ${beacon.getDeviceNetworkId()})\n"
     }
-    return dynamicPage(name: "listBeaconsPage", title: "Beacons", install: false, nextPage: mainPage) {
+    return dynamicPage(name: "listBeaconsPage", title: "List Beacons", install: false, nextPage: mainPage) {
         section() {
+            paragraph "The following beacon devices were added using the 'Add New Beacon' feature, and are setup to interface with the selected gateway."
             paragraph message
         }
     }
@@ -59,22 +58,22 @@ def listBeaconsPage() {
 def addBeaconPage() {
 	def newBeacons = [:]
 	state.beacons.each { beacon ->
-		def isChild = getChildDevice(beacon.value.uuid)
-		if (!isChild) {
-			newBeacons["${beacon.value.uuid}"] = "${beacon.value.uuid}"
+		def isChild = getChildDevice(beacon.value.dni)
+		if (!isChild && beacon.value.present) {
+			newBeacons["${beacon.value.dni}"] = "${beacon.value.dni}"
 		}
 	}
     
-    return dynamicPage(name: "addBeaconPage", title: "Add Beacon", install: false, nextPage: addBeaconStatusPage) {
+    return dynamicPage(name: "addBeaconPage", title: "Add New Beacon", install: false, nextPage: addBeaconStatusPage) {
         section(){
+            paragraph "NOTE: Beacon must be actively broadcasting and detected by the gateway to appear in this list.  If the beacon does not appear and is actively broadcasting, wait a few seconds and then refresh this page and try again."
 			input ("selectedAddBeacon", "enum",
 				   required: true,
 				   multiple: false,
-				   title: "Select beacon to add (${newBeacons.size() ?: 0} new detected).\n\t" +
-				   "Total Beacons: ${state.beacons.size()}",
+				   title: "Select a beacon to add (${newBeacons.size() ?: 0} new beacons detected)",
 				   description: "Use the dropdown to select beacon.",
                    options: newBeacons)
-            input "selectedBeaconName", "string", required: true, title: "Beacon Name"
+            input "selectedBeaconName", "string", required: true, title: "Give this Beacon a Name"
         }
     }
 }
@@ -83,21 +82,25 @@ def addBeaconStatusPage() {
     def status = []
     logDebug("beacon: " + selectedAddBeacon)
     def isChild = getChildDevice(selectedAddBeacon)
+    def success = false
+    def err = ""
     if (!isChild) {
         try {
             addChildDevice("ajardolino3", "Bluetooth Beacon", selectedAddBeacon, ["label": selectedBeaconName])
-            status.push([uuid: selectedAddBeacon, success: true, error: null])
+            success = true
         }
-        catch(error) {
-            status.push([uuid: selectedAddBeacon, success: false, error: error])
+        catch(exception) {
+            error = exception
         }
+    }
+    if(success) {
+        message = "New beacon added successfully (" + selectedBeaconName + ")."
+    } else {
+        message = "Unable to add beacon: " + err;
     }
 	app?.removeSetting("selectedAddBeacon")
 	app?.removeSetting("selectedBeaconName")
-    def message = ""
-    status.each{ result ->
-        message += "\t${result}\n"
-    }
+    
 	return dynamicPage(name:"addBeaconStatusPage",
 					   title: "Add Beacon Status",
 					   nextPage: mainPage,
@@ -127,8 +130,6 @@ def uninstalled() {
 }
 
 mappings {
-    // The root path - you can also map other paths or use parameters in paths and posted data
-    path("/beacons") { action: [GET: "getBeacons"]}
     path("/gateway") { action: [POST: "postGateway"] }
 }
 
@@ -151,62 +152,61 @@ def postGateway() {
     def slurper = new JsonSlurper()
     def obj = slurper.parseText(parsed)
     logDebug("Parsed payload from Gateway: " + obj)
-
-    logDebug("Beacon State BEFORE Update: " + state.beacons)
+    
+    state.beacons.each { beacon ->
+        beacon.value.present = false
+    }
+    
+    def detected = String.format("%tF %<tH:%<tM", java.time.LocalDateTime.now())
     obj.beacons.each { beacon ->
-        logDebug("beacon" + beacon.uuid + ", present: " + beacon.present)
-        def b = state.beacons.find{ c -> c == beacon.uuid }
-        if(b) {
-            logDebug("Update Beacon: " + beacon.uuid)
-            b.present = beacon.present
+        if(beacon.type)
+        {
+            switch(beacon.type) {
+                case "iBeacon":
+                    if(beacon.uuid instanceof String && beacon.major instanceof Integer && beacon.minor instanceof Integer) {
+                        beacon.dni = "${beacon.uuid}:${beacon.major}:${beacon.minor}"
+                        logDebug("iBeacon detected:" + beacon.dni)
+                    } else {
+                        logDebug("iBeacon type has invalid uuid, major, or minor types")
+                    }
+                    break
+                default:
+                    logDebug("unknown beacon type: " + beacon.type)
+                    break
+            }
         }
-        else {
-            logDebug("Create New Beacon: " + beacon.uuid)
-            b = createBeacon(beacon.uuid, beacon.present)
-            state.beacons["${beacon.uuid}"] = b
+        if(beacon.dni)
+        {
+            def b = state.beacons["${beacon.dni}"]
+            if(!b) {
+                b = [:]
+                b.dni = beacon.dni
+            }
+            b.present = true
+            b.detected = detected
+            state.beacons["${beacon.dni}"] = b
         }
     }
-    logDebug("Beacon State AFTER Update: " + state.beacons)
     
 	state.beacons.each { beacon ->
-        logDebug("Check Beacon: " + beacon.value.uuid)
-		def isChild = getChildDevice(beacon.value.uuid)
+        //if(beacon.value.detected) logDebug("last detected: " + Date.parse("yyyy-MM-dd HH:mm", beacon.value.detected))
+        def b = beacon.value
+		def isChild = getChildDevice(b.dni)
 		if (isChild) {
-            logDebug("Child Beacon: " + beacon.value.uuid + ", present: " + isChild.currentValue("presence"))
-            if(beacon.value.present) {
-                isChild.sendEvent(name: "presence", value: "present")
-            } else {
+            if(isChild.currentValue("presence") == "present" && !b.present) {
+                logDebug("Beacon: " + b.dni + ", presence: not present")
                 isChild.sendEvent(name: "presence", value: "not present")
+            }
+            else if(isChild.currentValue("presence") == "not present" && b.present) {
+                logDebug("Beacon: " + b.dni + ", presence: present")
+                isChild.sendEvent(name: "presence", value: "present")
             }
     	}
     }
 }
 
-def createBeacon(uuid, present) {
-    def beacon = [:]
-    beacon.uuid = uuid
-    beacon.present = present
-    return beacon
-}
-
-def getBeacons() {
-    def obj = [:] //define map
-    obj.beacons = [] //define array 
-
-    childApps.each { child ->
-        def b = [:] 
-        def presence = child.getSetting("presence")
-        b.id = presence.id
-        b.name = child.label
-        b.uuid = child.getSetting("uuid")
-        obj.beacons.push(b)
-    }
-    
-    def result = JsonOutput.toJson(obj)
-    render contentType: "application/json", data: result, status: 200
-}
-
 def logDebug(msg) {
     if(debugLog) log.debug(msg)
 }
+
 
--- april-brother-ble-gateway.groovy
+++ april-brother-ble-gateway.groovy
@@ -13,14 +13,40 @@ metadata {
 }
 
 def parsePayload(payload) {
-    log.debug("parsePayload received: " + payload)
+    logDebug("parsePayload received: " + payload)
     def slurper = new JsonSlurper()
     def parsed = slurper.parseText(payload)
-    parsed.beacons[0].uuid = parsed.beacons[0].uuid + "abc"
-    def json = JsonOutput.toJson(parsed)
+
+    def obj = [:]
+    obj.beacons = []
+
+    parsed.devices.each{ device ->
+        def type = device[0]
+        def mac = device[1]
+        def rssi = device[2]
+        def data = device[3]
+        if(data.length() == 60) {
+            def uuid = data.substring(18,18+32)
+            def major = Integer.parseInt(data.substring(18+32,18+32+4),16)
+            def minor = Integer.parseInt(data.substring(18+32+4,18+32+8),16)
+            log.debug("iBeacon detected: " + uuid + ", major: " + major + ", minor: " + minor)
+            def beacon = [:]
+            beacon.type = "iBeacon"
+            beacon.uuid = uuid
+            beacon.major = major
+            beacon.minor = minor
+            obj.beacons.push(beacon)
+        }
+    }
+                  
+    def json = JsonOutput.toJson(obj)
     updateDataValue("parsed", json)
-    log.debug("parsePayload parsed: " + json)
+    logDebug("parsePayload parsed: " + json)
 }
 
 def installed() {
+}
+
+def logDebug(msg) {
+    log.debug(msg)
 }
