a3da714cea79d18eefbe5a97391d7fa7ae3ef191
BPTWorld <bryan@bptworld.com>
2023-12-11 20:31:38 -0500

    Update ble-beacon.groovy

--- ble-beacon.groovy
+++ ble-beacon.groovy
@@ -4,7 +4,7 @@ def deviceVersion() { return "1.3.1" }
 
 metadata {
 	definition (
-        name: "BLE Beacon", 
+        name: "BLE Beacon Modified", 
         namespace: "ajardolino3", 
         author: "Art Ardolino")
     {
@@ -18,6 +18,7 @@ metadata {
         attribute "distance", "number"
         attribute "type", "string"
         attribute "since", "string"
+        attribute "depCheck", "number"
 	}
     preferences() {    	
         section(""){
@@ -33,6 +34,7 @@ def installed() {
     sendEvent(name: "distance", value: 999999999)
     sendEvent(name: "type", value: "unknown")
     sendEvent(name: "since", value: "unknown")
+    sendEvent(name: "depCheck", value: 0)
 }
 
 def arrived() {
@@ -49,5 +51,6 @@ def departed() {
     sendEvent(name: "distance", value: 999999999)
     def theWhen = new Date()
     sendEvent(name: "since", value: theWhen)
+    sendEvent(name: "depCheck", value: 0)
     if(logEnable) log.info "Beacon has Departed"
 }
